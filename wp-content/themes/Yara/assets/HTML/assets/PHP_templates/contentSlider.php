3af881fec03b437f0745a14f02a13e24eb63300b
vvvicho <vicho.vichev@yara.com>
2023-08-18 12:37:08 +0300

    homepage add content in to slider

--- wp-content/themes/Yara/assets/HTML/assets/PHP_templates/contentSlider.php
+++ wp-content/themes/Yara/assets/HTML/assets/PHP_templates/contentSlider.php
@@ -9,7 +9,24 @@
                     <p>I like to use satellite imagery to scout for any management issues.</p>
                     <p>The maps are a very visual way of picking out differences in growth and development to
                         help prioritise decisions.”</p>
-                    <p class="flag--uk">Jorin Grimsdale, Farmer, UK</p>
+                    <p class="flafIcons flafIcons--uk">Jorin Grimsdale, Farmer, UK</p>
+                </li>
+                <li>
+                    <p>“Nitrogen Use Efficiency is one of the most significant challenges mankind faces if we are to reduce carbon and nutrient emissions, yet feed a growing global population.</p>
+                    <p>The Yara N-Tester is a valuable and practical tool for farmers to use, backed by decades of research results, to improve efficiency.”</p>
+
+                    <p class="flafIcons flafIcons--uk">Dr Alastair Leake Director of Policy at the Game & Wildlife Conservation Trust, UK</p>
+                </li>
+                <li>
+                    <p>“It's much better than the solutions that we saw in the past, and we also can see the field from above and identify problematic areas or that have some deficiencies.”</p>
+
+                    <p class="flafIcons flafIcons--it">Fabiano Paganella Agronomist, Italy</p>
+                </li>
+                <li>
+
+
+                    <p>“If you want to take the first step towards precision agriculture, feel free to test to create simulations on Atfarm. This will allow you to understand the potential of this tool.”</p>
+                    <p class="flafIcons flafIcons--fr">Remy Dumery, Farmer, France</p>
                 </li>
                 <li>
                     <p>“Atfarm is very simple to use, taking just 10 mins to load all my field boundaries over
@@ -17,56 +34,31 @@
                     <p>I like to use satellite imagery to scout for any management issues.</p>
                     <p>The maps are a very visual way of picking out differences in growth and development to
                         help prioritise decisions.”</p>
-                    <p class="flag--uk">Jorin Grimsdale, Farmer, UK</p>
+                    <p class="flafIcons flafIcons--uk">Jorin Grimsdale, Farmer, UK</p>
                 </li>
                 <li>
-                    <p>“Atfarm is very simple to use, taking just 10 mins to load all my field boundaries over
-                        the 2200 hectares of farming.</p>
-                    <p>I like to use satellite imagery to scout for any management issues.</p>
-                    <p>The maps are a very visual way of picking out differences in growth and development to
-                        help prioritise decisions.”</p>
-                    <p class="flag--uk">Jorin Grimsdale, Farmer, UK</p>
+                    <p>“Nitrogen Use Efficiency is one of the most significant challenges mankind faces if we are to reduce carbon and nutrient emissions, yet feed a growing global population.</p>
+                    <p>The Yara N-Tester is a valuable and practical tool for farmers to use, backed by decades of research results, to improve efficiency.”</p>
+
+                    <p class="flafIcons flafIcons--uk">Dr Alastair Leake Director of Policy at the Game & Wildlife Conservation Trust, UK</p>
                 </li>
                 <li>
-                    <p>“Atfarm is very simple to use, taking just 10 mins to load all my field boundaries over
-                        the 2200 hectares of farming.</p>
-                    <p>I like to use satellite imagery to scout for any management issues.</p>
-                    <p>The maps are a very visual way of picking out differences in growth and development to
-                        help prioritise decisions.”</p>
-                    <p class="flag--uk">Jorin Grimsdale, Farmer, UK</p>
+                    <p>“It's much better than the solutions that we saw in the past, and we also can see the field from above and identify problematic areas or that have some deficiencies.”</p>
+
+                    <p class="flafIcons flafIcons--it">Fabiano Paganella Agronomist, Italy</p>
                 </li>
                 <li>
-                    <p>“Atfarm is very simple to use, taking just 10 mins to load all my field boundaries over
-                        the 2200 hectares of farming.</p>
-                    <p>I like to use satellite imagery to scout for any management issues.</p>
-                    <p>The maps are a very visual way of picking out differences in growth and development to
-                        help prioritise decisions.”</p>
-                    <p class="flag--uk">Jorin Grimsdale, Farmer, UK</p>
+
+
+                    <p>“If you want to take the first step towards precision agriculture, feel free to test to create simulations on Atfarm. This will allow you to understand the potential of this tool.”</p>
+                    <p class="flafIcons flafIcons--fr">Remy Dumery, Farmer, France</p>
                 </li>
-                <li>
-                    <p>“Atfarm is very simple to use, taking just 10 mins to load all my field boundaries over
-                        the 2200 hectares of farming.</p>
-                    <p>I like to use satellite imagery to scout for any management issues.</p>
-                    <p>The maps are a very visual way of picking out differences in growth and development to
-                        help prioritise decisions.”</p>
-                    <p class="flag--uk">Jorin Grimsdale, Farmer, UK</p>
-                </li>
-                <li>
-                    <p>“Atfarm is very simple to use, taking just 10 mins to load all my field boundaries over
-                        the 2200 hectares of farming.</p>
-                    <p>I like to use satellite imagery to scout for any management issues.</p>
-                    <p>The maps are a very visual way of picking out differences in growth and development to
-                        help prioritise decisions.”</p>
-                    <p class="flag--uk">Jorin Grimsdale, Farmer, UK</p>
-                </li>
-                <li>
-                    <p>“Atfarm is very simple to use, taking just 10 mins to load all my field boundaries over
-                        the 2200 hectares of farming.</p>
-                    <p>I like to use satellite imagery to scout for any management issues.</p>
-                    <p>The maps are a very visual way of picking out differences in growth and development to
-                        help prioritise decisions.”</p>
-                    <p class="flag--uk">Jorin Grimsdale, Farmer, UK</p>
-                </li>
+
+
+
+
+
+
 
             </ul>
         </div>
