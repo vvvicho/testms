c987b7a8961f149fa8b2a4cfa388452e61849a53
vvvicho <vicho.vichev@yara.com>
2023-08-28 10:36:32 +0300

    header fixes

--- wp-content/themes/Yara/assets/HTML/assets/PHP_templates/yaraSolutionsNutrition/yaraHead.php
+++ wp-content/themes/Yara/assets/HTML/assets/PHP_templates/yaraSolutionsNutrition/yaraHead.php
@@ -2,9 +2,9 @@
         <div class="titleBlock">
             <div>
                 <p>
-                    <strong>Monitor crop growth</strong><br>
-                    <strong>anywhere, anytime</strong><br>
-                    Discover the areas of high potential and crop variability on your fields in 1 click.
+                    <strong>Create a Nutrition Plan</strong><br>
+                    <strong>in just a few clicks</strong><br>
+                    Prepare for whatever the season brings with Atfarm’s Nutrition Plan. 
                 </p>
                 <div class="button button-dark">Learn more</div>
             </div>
