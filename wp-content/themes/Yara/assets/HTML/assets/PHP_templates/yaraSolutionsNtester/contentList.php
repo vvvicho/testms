25251cc2ac87f5107445b7386ccce2eefc063bac
vvvicho <vicho.vichev@yara.com>
2023-08-29 08:59:15 +0300

    fix icons and positions

--- wp-content/themes/Yara/assets/HTML/assets/PHP_templates/yaraSolutionsNtester/contentList.php
+++ wp-content/themes/Yara/assets/HTML/assets/PHP_templates/yaraSolutionsNtester/contentList.php
@@ -1,32 +1,35 @@
 <section class="bgColors--sectionLight">
-            <div class="warper">
-                <div class="item_12">
-                    <h3>What can Atfarm’s Nutrition Planner do for you?</h3>
+    <div class="warper">
+        <div class="item_12">
+            <h3>What can Atfarm’s Nutrition Planner do for you?</h3>
+        </div>
+        <figure class="item_7">
+            <img src="../HTML/assets/images/ntester.webp" width="677" height="508" alt="my image">
+        </figure>
+        <div class="articleInfo item_5">
+            <div class="articleContent">
+                <p>QFind out how much nitrogen (N) to apply to your crops this season.</p>
+                <div class="button button-dark">
+                    Get your N-Tester BT
                 </div>
-                <figure class="item_7">
-                    <img src="../HTML/assets/images/ntester.webp" width="677" height="508" alt="my image">
-                </figure>
-                <div class="articleInfo item_5">
-                    <div class="articleContent">                        
-                        <p>QFind out how much nitrogen (N) to apply to your crops this season.</p>
-                        <div class="button button-dark">
-                            Get your N-Tester BT 
-                        </div>
-                    </div>
-                </div>
-                <div class="item_12 box box--bgColor--blue twoColMid">
-                    <div>
-                        <h4>Easy and affordable</h4>
-                        <p>Maximise return on N applied and improve yield quality with field specific N-rate recommendations based on the real N-uptake of your cereals.</p>
-                    </div>
-                    <div>
-                        <h4>Agronomically robust</h4>
-                        <p>The N-Tester BT has been field tested across hundreds of trials and developed by Yara Research & Development for over 25 years. </p>
-                    </div>                    
-                </div>
-                <!--div class="item_12 articleNavigation">
+            </div>
+        </div>
+
+        <!--div class="item_12 articleNavigation">
                     <div class="icons icons-navLeft"></div>
                     <div class="icons icons-navRight"></div>
                 </div-->
+    </div>
+    <div class="warper warper--boxGallery">
+            <div class="item_12 box box--bgColor--blue twoColMid">
+                <div class="icons-tester">
+                    <h4>Easy and affordable</h4>
+                    <p>Maximise return on N applied and improve yield quality with field specific N-rate recommendations based on the real N-uptake of your cereals.</p>
+                </div>
+                <div class="icons-clock">
+                    <h4>Agronomically robust</h4>
+                    <p>The N-Tester BT has been field tested across hundreds of trials and developed by Yara Research & Development for over 25 years. </p>
+                </div>
             </div>
-        </section>
+        </div>
+</section>
