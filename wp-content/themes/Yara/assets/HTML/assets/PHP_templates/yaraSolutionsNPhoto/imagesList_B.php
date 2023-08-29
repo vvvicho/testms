87cc1336f2e7071fac032eaaab3487ba4d8e89de
vvvicho <vicho.vichev@yara.com>
2023-08-29 09:46:24 +0300

    replace cap with h4

--- wp-content/themes/Yara/assets/HTML/assets/PHP_templates/yaraSolutionsNPhoto/imagesList_B.php
+++ wp-content/themes/Yara/assets/HTML/assets/PHP_templates/yaraSolutionsNPhoto/imagesList_B.php
@@ -5,36 +5,41 @@
                 <div class="galleryItem">
                     <figure>
                         <img src="../HTML/assets/images/m1.webp" width="208" height="278" alt="my image">
-                        <figcaption>Tillering</figcaption>                        
+                                               
                     </figure>
+                    <h4>Tillering</h4> 
                     <p>1st application </p>
                 </div>
                 <div class="galleryItem galleryItem--bgColor">
                     <figure>
                         <img src="../HTML/assets/images/m2.webp" width="208" height="278" alt="my image">
-                        <figcaption>Stem elongation</figcaption>                        
+                                              
                     </figure>
+                    <h4>Stem elongation</h4>  
                     <p>2nd application</p>
                 </div>
                 <div class="galleryItem galleryItem--bgColor">
                     <figure>
                         <img src="../HTML/assets/images/m3.webp" width="208" height="278" alt="my image">
-                        <figcaption>Flag leaf development</figcaption>                        
+                                             
                     </figure>
+                    <h4>Flag leaf development</h4>   
                     <p>3rd application </p>
                 </div>
                 <div class="galleryItem galleryItem--bgColor">
                     <figure>
                         <img src="../HTML/assets/images/m4.webp" width="208" height="278" alt="my image">
-                        <figcaption>Spike development</figcaption>                        
+                                               
                     </figure>
+                    <h4>Spike development</h4> 
                     <p>Protein dressing  </p>
                 </div>
                 <div class="galleryItem">
                     <figure>
                         <img src="../HTML/assets/images/m5.webp" width="208" height="278" alt="my image">
-                        <figcaption>WHarvest</figcaption>                        
+                                            
                     </figure>
+                    <h4>WHarvest</h4>    
                 </div>
                 <div class="item_5 buttonHolder">
                     <div class="button button-dark">
