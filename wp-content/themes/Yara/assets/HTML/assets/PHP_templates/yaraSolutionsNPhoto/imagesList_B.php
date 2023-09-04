f8309012a5d7be437fe42841a6374d2570d2a3da
vvvicho <vicho.vichev@yara.com>
2023-09-04 12:00:24 +0300

    change list items

--- wp-content/themes/Yara/assets/HTML/assets/PHP_templates/yaraSolutionsNPhoto/imagesList_B.php
+++ wp-content/themes/Yara/assets/HTML/assets/PHP_templates/yaraSolutionsNPhoto/imagesList_B.php
@@ -1,8 +1,8 @@
-<section class="bgColors--sectionLight mhide thide">
+<section class="bgColors--sectionLNormal mhide thide">
             <div class="warper warper--gallery warper--gallery--five warper--gallery--bordered">
                 <h3 class="item_5">When should I use my N-Tester BT?</h3>
                 <h4 class="item_5">Perfect the n-rate for your 1st application during tillering.</h4>
-                <div class="galleryItem">
+                <div class="galleryItem  galleryItem--bgColorYello">
                     <figure>
                         <img src="../HTML/assets/images/m1.webp" width="208" height="278" alt="my image">
                                                
@@ -10,7 +10,7 @@
                     <h4>Tillering</h4> 
                     <p>1st application </p>
                 </div>
-                <div class="galleryItem galleryItem--bgColor">
+                <div class="galleryItem   galleryItem--bgColorWhite">
                     <figure>
                         <img src="../HTML/assets/images/m2.webp" width="208" height="278" alt="my image">
                                               
@@ -18,7 +18,7 @@
                     <h4>Stem elongation</h4>  
                     <p>2nd application</p>
                 </div>
-                <div class="galleryItem galleryItem--bgColor">
+                <div class="galleryItem   galleryItem--bgColorWhite">
                     <figure>
                         <img src="../HTML/assets/images/m3.webp" width="208" height="278" alt="my image">
                                              
@@ -26,7 +26,7 @@
                     <h4>Flag leaf development</h4>   
                     <p>3rd application </p>
                 </div>
-                <div class="galleryItem galleryItem--bgColor">
+                <div class="galleryItem   galleryItem--bgColorWhite">
                     <figure>
                         <img src="../HTML/assets/images/m4.webp" width="208" height="278" alt="my image">
                                                
@@ -34,12 +34,12 @@
                     <h4>Spike development</h4> 
                     <p>Protein dressing  </p>
                 </div>
-                <div class="galleryItem">
+                <div class="galleryItem   galleryItem--bgColorWhite">
                     <figure>
                         <img src="../HTML/assets/images/m5.webp" width="208" height="278" alt="my image">
                                             
                     </figure>
-                    <h4>WHarvest</h4>    
+                    <h4>Harvest</h4>    
                 </div>
                 <div class="item_5 buttonHolder">
                     <div class="button button-dark">
--- wp-content/themes/Yara/assets/HTML/assets/PHP_templates/yaraSolutionsNtester/imagesList_B.php
+++ wp-content/themes/Yara/assets/HTML/assets/PHP_templates/yaraSolutionsNtester/imagesList_B.php
@@ -1,21 +1,21 @@
 <section class="bgColors--sectionLight mhide thide">
             <div class="warper warper--gallery warper--gallery--five warper--gallery--bordered">
                 <h3 class="item_5">Calibrated for most cereals</h3>
-                <div class="galleryItem">
+                <div class="galleryItem  galleryItem--bgColorWhite">
                     <figure>
                         <img src="../HTML/assets/images/m1.webp" width="208" height="278" alt="my image">                                               
                     </figure>
                     <h4>Tillering</h4> 
                     <p>1st application </p>
                 </div>
-                <div class="galleryItem galleryItem--bgColor">
+                <div class="galleryItem galleryItem--bgColorYello">
                     <figure>
                         <img src="../HTML/assets/images/m2.webp" width="208" height="278" alt="my image">                                              
                     </figure>
                     <h4>Stem elongation</h4>  
                     <p>2nd application</p>
                 </div>
-                <div class="galleryItem galleryItem--bgColor">
+                <div class="galleryItem galleryItem--bgColorYello">
                     <figure>
                         <img src="../HTML/assets/images/m3.webp" width="208" height="278" alt="my image">
                                                 
@@ -23,7 +23,7 @@
                     <h4>Flag leaf development</h4>
                     <p>3rd application </p>
                 </div>
-                <div class="galleryItem galleryItem--bgColor">
+                <div class="galleryItem galleryItem--bgColorYello">
                     <figure>
                         <img src="../HTML/assets/images/m4.webp" width="208" height="278" alt="my image">
                                                
@@ -31,7 +31,7 @@
                     <h4>Spike development</h4> 
                     <p>Protein dressing  </p>
                 </div>
-                <div class="galleryItem">
+                <div class="galleryItem  galleryItem--bgColorWhite">
                     <figure>
                         <img src="../HTML/assets/images/m5.webp" width="208" height="278" alt="my image">                                              
                     </figure>
