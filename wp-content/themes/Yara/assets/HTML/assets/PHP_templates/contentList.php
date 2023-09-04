44738f2070a7e097ffa387b4a49d2a3c741a80e8
vvvicho <vicho.vichev@yara.com>
2023-09-04 15:05:58 +0300

    slider

--- wp-content/themes/Yara/assets/HTML/assets/PHP_templates/contentList.php
+++ wp-content/themes/Yara/assets/HTML/assets/PHP_templates/contentList.php
@@ -1,13 +1,25 @@
 <section id="contentList_001" class="bgColors--sectionLight">
-<link rel="preload" as="image" href="../HTML/assets/images/slider/fetizerOptimize_1.webp?z=11" />
-<link rel="preload" as="image" href="../HTML/assets/images/slider/fetizerOptimize_2.webp?z=11" />
-<link rel="preload" as="image" href="../HTML/assets/images/slider/fetizerOptimize_3.webp?z=11" />
+            <style>
+                .fade-out {
+                    opacity: 0;
+                    transition: 0.5s;
+                }
+
+                .fade-in {
+                    opacity: 1;
+                    transition: 0.5s;
+                }
+            </style>
+            <link rel="preload" as="image" href="../HTML/assets/images/slider/fetizerOptimize_1.webp?z=11" />
+            <link rel="preload" as="image" href="../HTML/assets/images/slider/fetizerOptimize_2.webp?z=11" />
+            <link rel="preload" as="image" href="../HTML/assets/images/slider/fetizerOptimize_3.webp?z=11" />
             <div class="warper">
                 <div class="item_12">
                     <h3>How can Atfarm help optimise your fertiliser strategy?</h3>
                 </div>
                 <figure class="item_7">
-                    <img src="../HTML/assets/images/slider/fetizerOptimize.webp?z=11" width="677" height="508" alt="my image">
+                    <img src="../HTML/assets/images/slider/fetizerOptimize.webp?z=11" width="677" height="508"
+                        alt="my image">
                 </figure>
                 <div class="articleInfo item_5">
                     <div class="articleContent">
@@ -33,26 +45,54 @@
                 let sectionDataSubTitle = sectionData.querySelector("h4");
                 let sectionDataImage = sectionData.querySelector("figure img");
                 let sectionDataContent = sectionData.querySelector("p");
+                let sectionButton = sectionData.querySelector(".button");
 
 
                 function slideLeftRight(direction) {
                     let maxObjects = dataArticleContainer.length;
                     console.log(maxObjects);
                     currentitem += direction;
-                    if (currentitem < 0 ){
+                    if (currentitem < 0) {
 
-                        currentitem = maxObjects -1;
+                        currentitem = maxObjects - 1;
 
-                    }else if (currentitem >= maxObjects ) {
+                    } else if (currentitem >= maxObjects) {
                         currentitem = 0;
 
                     }
 
                     console.log(currentitem);
 
-                    sectionDataSubTitle.innerHTML = dataArticleContainer[currentitem].subtitle;
-                    sectionDataContent.innerHTML = dataArticleContainer[currentitem].content;
-                    sectionDataImage.setAttribute("src",dataArticleContainer[currentitem].image);
+
+                    sectionDataSubTitle.classList.add("fade-out");
+                    sectionDataContent.classList.add("fade-out");
+                    sectionDataImage.classList.add("fade-out");
+                    sectionButton.classList.add("fade-out");
+                    sectionDataSubTitle.classList.remove("fade-in");
+                    sectionDataContent.classList.remove("fade-in");
+                    sectionDataImage.classList.remove("fade-in");
+                    sectionButton.classList.remove("fade-in");
+
+
+
+
+                    setTimeout(() => {
+                        sectionDataSubTitle.innerHTML = dataArticleContainer[currentitem].subtitle;
+                        sectionDataContent.innerHTML = dataArticleContainer[currentitem].content;
+                        sectionDataImage.setAttribute("src", dataArticleContainer[currentitem].image);
+                        sectionDataSubTitle.classList.remove("fade-out");
+                        sectionDataContent.classList.remove("fade-out");
+                        sectionDataImage.classList.remove("fade-out");
+                        sectionButton.classList.remove("fade-out");
+                        sectionDataSubTitle.classList.add("fade-in");
+                        sectionDataContent.classList.add("fade-in");
+                        sectionDataImage.classList.add("fade-in");
+                        sectionButton.classList.add("fade-in");
+                    }, "500");
+
+
+
+
 
 
                 }
@@ -60,34 +100,34 @@
                 let slideLeftButton = document.getElementById("contentList_001_left");
                 let slideRightButton = document.getElementById("contentList_001_right");
 
-                slideLeftButton.onclick = function() {
+                slideLeftButton.onclick = function () {
                     slideLeftRight(-1);
                 };
 
-                slideRightButton.onclick = function() {
+                slideRightButton.onclick = function () {
                     slideLeftRight(1);
                 };
 
 
-                dataArticleContainer[0] = 
+                dataArticleContainer[0] =
                 {
                     image: "../HTML/assets/images/slider/fetizerOptimize.webp?z=11",
                     subtitle: "Optimise nitrogen use efficiency ",
                     content: "Improve crop growth and evenness by utilising the in-field measurements for variable nitrogen applications."
                 };
-                dataArticleContainer[1] = 
+                dataArticleContainer[1] =
                 {
                     image: "../HTML/assets/images/slider/fetizerOptimize_1.webp?z=11",
                     subtitle: "Measure N-demand ",
                     content: "Analyse your crop throughout the season with accurate nitrogen uptake values captured by N-Photo analysis and the N-Tester BT device."
                 };
-                dataArticleContainer[2] = 
+                dataArticleContainer[2] =
                 {
                     image: "../HTML/assets/images/slider/fetizerOptimize_2.webp?z=11",
                     subtitle: "Create a holistic Nutrition Plan ",
                     content: "Start every season with Nutrition Planning that gives you more than just calculations. Get the nutrition insights you need to reach your yield goals. "
                 };
-                dataArticleContainer[3] = 
+                dataArticleContainer[3] =
                 {
                     image: "../HTML/assets/images/slider/fetizerOptimize_3.webp?z=11",
                     subtitle: "Track crop growth",
