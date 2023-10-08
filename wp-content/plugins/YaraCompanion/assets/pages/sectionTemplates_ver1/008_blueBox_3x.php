a16addb9fe82cd9b5434852930f34059b3b68923
vvvicho <vicho.vichev@yara.com>
2023-10-08 14:22:42 +0300

    add blue box template

---
+++ wp-content/plugins/YaraCompanion/assets/pages/sectionTemplates_ver1/008_blueBox_3x.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Section Name: Gallery
+ * Data Tape: template gallery
+ * Version: 1.0.0
+ * Author: Yara Team
+ * Author URI: https://yara.com
+ */
+?>
+
+<li class="adminHolder--leftNav--templatesList--button" onClick="insertMySection(this)" data-type="template gallery">
+    <figure>
+        <img src="/wp-content/plugins/YaraCompanion/assets/imagesAdmin/box_x3.png" alt="section content">
+        <span>Blue Box x3</span>
+    </figure>
+    <templateHidden data-repeat="1" data-head="0" data-used="0">
+        <div class="bgColors--sectionLNormal section">
+            <div class="warper warper--boxGallery">
+                <div class="item_12 box box--bgColor--blue treeColMid">
+                    <div class="icons-searchBlue">
+                        <h4 data-edit="text delete">Easy to understand overview</h4>
+                        <p data-edit="text duplicate delete">Easy to understand overview of nutrient needs for the entire season and each growth stage.</p>
+                    </div>
+                    <div class="icons-crop">
+                        <h4 data-edit="text duplicate">Detailed information</h4>
+                        <p data-edit="text duplicate delete">View detailed information on crop nutrient demand and fertiliser supply to reach your target yield.</p>
+                    </div>
+                    <div class="icons-plans">
+                        <h4 data-edit="text delete">Free plans</h4>
+                        <p data-edit="text duplicate delete">Create unlimited plans for FREE! </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </templateHidden>
+</li>
