b19d454ae57f8606098ed49fcd6b6d1c912b218a
vvvicho <vicho.vichev@yara.com>
2023-10-09 10:59:54 +0300

    update blue box section x2

---
+++ wp-content/plugins/YaraCompanion/assets/pages/sectionTemplates_ver1/009_blueBox_2x.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Section Name: Gallery
+ * Data Tape: template gallery
+ * Version: 1.0.0
+ * Author: Yara Team
+ * Author URI: https://yara.com
+ */
+?>
+
+<li class="adminHolder--leftNav--templatesList--button" onClick="insertMySection(this)" data-type="template gallery">
+    <figure>
+        <img src="/wp-content/plugins/YaraCompanion/assets/imagesAdmin/box_x2.png" alt="section content">
+        <span>Blue Box x2</span>
+    </figure>
+    <templateHidden data-repeat="1" data-head="0" data-used="0">
+    <section class="bgColors--sectionLight">
+    <div class="warper warper--boxGallery">
+            <div class="item_12 box box--bgColor--blue twoColMid">
+                <div class="icons-tester">
+                    <h4 data-edit="text  delete">Easy and affordable</h4>
+                    <p data-edit="text duplicate delete">Maximise return on N applied and improve yield quality with field specific N-rate recommendations based on the real N-uptake of your cereals.</p>
+                </div>
+                <div class="icons-clock">
+                    <h4 data-edit="text  delete">Agronomically robust</h4>
+                    <p data-edit="text duplicate delete">The N-Tester BT has been field tested across hundreds of trials and developed by Yara Research &amp; Development for over 25 years. </p>
+                </div>
+            </div>
+        </div>
+</section>
+
+    </templateHidden>
+</li>
