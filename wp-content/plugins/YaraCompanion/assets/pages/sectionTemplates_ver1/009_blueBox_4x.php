286687f970be733cc13812b48f4535d6c8efd9a3
vvvicho <vicho.vichev@yara.com>
2023-10-10 12:03:24 +0300

    add new section blue box x4

---
+++ wp-content/plugins/YaraCompanion/assets/pages/sectionTemplates_ver1/009_blueBox_4x.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Section Name: Gallery
+ * Data Tape: template gallery
+ * Version: 1.0.0
+ * Author: Yara Team
+ * Author URI: https://yara.com
+ */
+?>
+
+<li class="adminHolder--leftNav--templatesList--button" onClick="insertMySection(this)" data-type="template gallery">
+    <figure>
+        <img src="/wp-content/plugins/YaraCompanion/assets/imagesAdmin/box_x4.png" alt="section content">
+        <span>Blue Box x4</span>
+    </figure>
+    <templateHidden data-repeat="1" data-head="0" data-used="0">
+        <section class="bgColors--sectionLNormal">
+            <div class="warper warper--boxGallery">
+                <div class="item_12 box box--bgColor--blue fourColMid">
+                    <div class="icons-upDiagram">
+                        <h4 data-edit="text delete">1987</h4>
+                        <p data-edit="text duplicate delete">First rigorous field trials of Variable N and P application show improvements in profitability.
+                        </p>
+                    </div>
+                    <div class="icons-document">
+                        <h4 data-edit="text delete">1991</h4>
+                        <p data-edit="text duplicate delete">First instance of Precision Farming used in scientific literature.</p>
+                    </div>
+                    <div class="icons-meter">
+                        <h4 data-edit="text delete">1999</h4>
+                        <p data-edit="text duplicate delete">Create unlimited plans Yara’s tractor-mounted N-Sensor device created.</p>
+                    </div>
+                    <div class="icons-earth">
+                        <h4 data-edit="text delete">2018</h4>
+                        <p data-edit="text duplicate delete">Atfarm founded. Precision farming tools improved by combining N-Sensor technology and data from Sentinel 2 satellites.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        </div>
+
+    </templateHidden>
+</li>
